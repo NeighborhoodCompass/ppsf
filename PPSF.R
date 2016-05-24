ba7f1f6888d999c363157f1f0f8607a29f09869d
John K <john.killeen@durhamnc.gov>
2016-05-24 15:12:48 -0400

    filtering commercial multi-unit apartments

--- PPSF.R
+++ PPSF.R
@@ -1,6 +1,8 @@
 ##PPSF_Tax
 setwd("C:/Users/johnki/Desktop/Compass_Development/PPSF")
 PPSF <- read.csv("AMDTSL15_2.csv", header = TRUE, stringsAsFactors=FALSE, sep = ",")
+bg <- read.csv("bg.csv", header = TRUE, stringsAsFactors=FALSE, sep = ",")
+colnames(bg) <- c("id")
 
 require(lubridate)
 require(reshape)
@@ -8,8 +10,8 @@ require(dplyr)
 require(plyr)
 
 # format year and id fields, calculate per square foot values, make field for generating counts
-P <- mutate(PPSF, YR = substr(PPSF$AMDTSL,1,4), GEOID = as.character(GEOID10), YEAR = as.integer(YR), PPSF_H = PPSF$AMSLAM/PPSF$XXHAUN,PPSF_F = PPSF$AMSLAM/PPSF$AHFNAR, CT = 1)
-PP <- subset(P, select = -c(YR), YEAR > 2004)
+P <- mutate(PPSF, YR = substr(PPSF$AMDTSL,1,4), GEOID = as.character(GEOID10), YEAR = as.integer(YR), BUILT = as.integer(AHACYR), PPSF_H = PPSF$AMSLAM/PPSF$XXHAUN,PPSF_F = PPSF$AMSLAM/PPSF$AHFNAR, CT = 1)
+PP <- subset(P, select = -c(YR), YEAR > 2004 & BUILT > 0 & AMVICD == 'I' & AHBED_ > 0)
 
 
 #creat adj factor using averaged annual housing cpi from here: http://data.bls.gov/cgi-bin/surveymost?cu
@@ -29,7 +31,7 @@ write.table(Padj, file = "tax_sales_20052015_2.csv", sep = ",", row.names = FALS
 bgct <- cast(GEOID ~ YEAR, data = Padj, fun = sum, value = "CT", na.rm = TRUE)
 
 ## create table of sale counts for neighborhoods for each year
-nhct <- cast(name_1 ~ YEAR, data = Padj, fun = sum, value = "CT", na.rm = TRUE)##create table of median BG values for each year
+nhct <- cast(name_1 ~ YEAR, data = Padj, fun = sum, value = "CT", na.rm = TRUE)
 
 
 # create table of median values for blockgroups
@@ -45,6 +47,11 @@ nhmed <- cast(name_1 ~ YEAR, data = Padj, fun = median, value = "ADJPPSF_H", na.
   combo_bg <- merge(bgmed, bgct, by = "GEOID")
   combo_nh <- merge(nhmed, nhct, by = "name_1")
   
+# create table of median sales values for blockgroups
+bgmedsale <- cast(GEOID ~ YEAR, data = Padj, fun = median, value = "SOLDADJ", na.rm = TRUE)
+colnames(bgmedsale) <- c("id","y_2005","y_2006","y_2007","y_2008","y_2009","y_2010","y_2011","y_2012","y_2013","y_2014","y_2015")
+medsale <- merge(bg, bgmedsale, by = "id", all=TRUE)  
+write.table(medsale, file = "bgmed_sale_tax20052015.csv", sep = ",", row.names = FALSE)  
   
 ##County medians
 P2 <- select(Padj, YEAR, AMSLAM, PPSF_H, SOLDADJ, cpi, adj_factor, ADJPPSF_H)
